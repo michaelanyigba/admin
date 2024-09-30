641972f7e0abd7eb8b8de2cf7c37dddfecf604bb
michaelanyigba <kwabenahlegacy18@gmail.com>
2024-09-30 06:29:56 -0700

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    outDir: 'dist', // This should point to the folder where your build is placed
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api":{
-        target: "https://liv-backend-2.onrender.com"
-      }
-    }
-  },
-})
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist',
-//   },
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       "/api":{
-//         target: "http://localhost:5000"
-//       }
-//     }
-//   },
-//   resolve: {
-//     alias: {
-//       // This tells Vite to use a browser-compatible polyfill or no-op module
-//       util: 'rollup-plugin-node-polyfills/polyfills/util'
-//     }
-//   },
-//   optimizeDeps: {
-//     exclude: ['util'] // Exclude the 'util' module from dependency optimization
-//   }
-// })
+  plugins: [],
+}
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,29 @@ export default defineConfig({
     outDir: 'dist', // This should point to the folder where your build is placed
   },
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api":{
+        target: "https://liv-backend-2.onrender.com"
+      }
+    }
+  },
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       "/api":{
+//         target: "http://localhost:5000"
+//       }
+//     }
+//   },
+// })
+
